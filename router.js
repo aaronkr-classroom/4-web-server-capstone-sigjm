2e723ecbd2d7f7590a5a037600476da3d0752b0e
정민김 <74294488+sigjm@users.noreply.github.com>
2024-03-27 03:09:27 +0000

    끝

---
+++ router.js
@@ -0,0 +1,31 @@
+//router.js
+"using strict";
+
+
+//Listing 7.6 (p. 123-124)
+const httpStatus = require('http-status-codes'),
+  contentTypes = require('./content-types'),
+  utils= require('./utils'),
+  routes = {
+    GET: {},
+    POST: {}
+  };
+
+// 라우트에 따른 콜백 함수를 처리하기 위한 함수 handle의 생성
+exports.handle = (req, res) => {
+  try{
+    routes[req.method][req.url](req, res);
+  }catch (e){
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile("views/error.html", res);
+    console.log("error: " + e);
+  }
+};
+
+// main.js로부터 routes에 등록하기 위한 get 및 post 함수 생성
+exports.get = (url, action) => {
+  routes["GET"][url] = action;
+};
+exports.post = (url, action) => {
+  routes["POST"][url] = action;
+};
